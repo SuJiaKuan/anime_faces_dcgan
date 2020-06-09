e6125361b151a13b33271969097585fab1f0fa61
SuJiaKuan <jk.su@ingenta.ai>
2020-06-09 23:44:18 +0900

    Add definitions for generator and discrminator

---
+++ simpsons_dcgan/model.py
@@ -0,0 +1,87 @@
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+
+    def __init__(self, size_z):
+        super(Generator, self).__init__()
+
+        self._network = nn.Sequential(
+            # From input z into 1024 x 4 x4.
+            nn.ConvTranspose2d(size_z,
+                               1024,
+                               4,
+                               stride=1,
+                               padding=0,
+                               bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(),
+            # From 1024 x 4 x4 into 512 x 8 x 8.
+            nn.ConvTranspose2d(1024,
+                               512,
+                               4,
+                               stride=2,
+                               padding=1,
+                               bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            # From 512 x 8 x 8 into 256 x 16 x 16.
+            nn.ConvTranspose2d(512,
+                               256,
+                               4,
+                               stride=2,
+                               padding=1,
+                               bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            # From 256 x 16 x 16 into 128 x 32 x 32.
+            nn.ConvTranspose2d(256,
+                               128,
+                               4,
+                               stride=2,
+                               padding=1,
+                               bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            # From 128 x 32 x 32 into 3 x 64 x 64
+            nn.ConvTranspose2d(128,
+                               3,
+                               4,
+                               stride=2,
+                               padding=1,
+                               bias=False),
+            nn.Tanh(),
+        )
+
+    def forward(self, input):
+        return self._network(input)
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self._network = nn.Sequential(
+            # From 3 x 64 x 64 into 128 x 32 x 32.
+            nn.Conv2d(3, 128, 4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # From 128 x 32 x 32 into 256 x 16 x 16.
+            nn.Conv2d(128, 256, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # From 256 x 16 x 16 into 512 x 8 x 8.
+            nn.Conv2d(256, 512, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            # From 512 x 8 x 8 into 1024 x 4 x 4.
+            nn.Conv2d(512, 1024, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            # From 1024 x 4 x 4 into.
+            nn.Conv2d(1024, 1, 4, stride=1, padding=0, bias=False),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, input):
+        return self._network(input)
