7b02e071cd347d863d78a75ced79ee3b9f5237c9
SuJiaKuan <jk.su@ingenta.ai>
2020-06-13 00:29:13 +0900

    Change filters size of network models

--- simpsons_dcgan/model.py
+++ simpsons_dcgan/model.py
@@ -7,25 +7,16 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         self._network = nn.Sequential(
-            # From input z into 1024 x 4 x4.
+            # From input z x 1 x 1 into 512 x 4 x4.
             nn.ConvTranspose2d(z_size,
-                               1024,
+                               512,
                                4,
                                stride=1,
                                padding=0,
                                bias=False),
-            nn.BatchNorm2d(1024),
-            nn.ReLU(),
-            # From 1024 x 4 x4 into 512 x 8 x 8.
-            nn.ConvTranspose2d(1024,
-                               512,
-                               4,
-                               stride=2,
-                               padding=1,
-                               bias=False),
             nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            # From 512 x 8 x 8 into 256 x 16 x 16.
+            nn.ReLU(),
+            # From 512 x 4 x4 into 256 x 8 x 8.
             nn.ConvTranspose2d(512,
                                256,
                                4,
@@ -34,7 +25,7 @@ class Generator(nn.Module):
                                bias=False),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
-            # From 256 x 16 x 16 into 128 x 32 x 32.
+            # From 256 x 8 x 8 into 128 x 16 x 16.
             nn.ConvTranspose2d(256,
                                128,
                                4,
@@ -43,8 +34,17 @@ class Generator(nn.Module):
                                bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
-            # From 128 x 32 x 32 into 3 x 64 x 64
+            # From 128 x 16 x 16 into 64 x 32 x 32.
             nn.ConvTranspose2d(128,
+                               64,
+                               4,
+                               stride=2,
+                               padding=1,
+                               bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            # From 64 x 32 x 32 into 3 x 64 x 64
+            nn.ConvTranspose2d(64,
                                3,
                                4,
                                stride=2,
@@ -63,23 +63,23 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
 
         self._network = nn.Sequential(
-            # From 3 x 64 x 64 into 128 x 32 x 32.
-            nn.Conv2d(3, 128, 4, stride=2, padding=1, bias=False),
+            # From 3 x 64 x 64 into 64 x 32 x 32.
+            nn.Conv2d(3, 64, 4, stride=2, padding=1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # From 128 x 32 x 32 into 256 x 16 x 16.
+            # From 64 x 32 x 32 into 128 x 16 x 16.
+            nn.Conv2d(64, 128, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            # From 128 x 16 x 16 into 256 x 8 x 8.
             nn.Conv2d(128, 256, 4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
-            # From 256 x 16 x 16 into 512 x 8 x 8.
+            # From 256 x 8 x 8 into 512 x 4 x 4.
             nn.Conv2d(256, 512, 4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True),
-            # From 512 x 8 x 8 into 1024 x 4 x 4.
-            nn.Conv2d(512, 1024, 4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.2, inplace=True),
-            # From 1024 x 4 x 4 into.
-            nn.Conv2d(1024, 1, 4, stride=1, padding=0, bias=False),
+            # From 512 x 4 x 4 into 1 x 1 x 1.
+            nn.Conv2d(512, 1, 4, stride=1, padding=0, bias=False),
             nn.Sigmoid(),
         )
 
